85182b43f2fc65f49657e31022635e16e597ce9f
parfait bazebi <parfait.noreply@gmail.com>
2023-09-05 16:40:28 +0200

    update object_detector

--- auto_ai/object_detector.py
+++ auto_ai/object_detector.py
@@ -75,7 +75,7 @@ def cropContour(image, center, max_distance):
     bottom = min([int(center[0] + max_distance + 1), height-1])
     left = max([int(center[1] - max_distance), 0])
     right = min([int(center[1] + max_distance+1), width-1])
-    print(left, right, top, bottom)
+    #print(left, right, top, bottom)
     return image[left:right, top:bottom]
 
 def findSigns(image, contours, threshold=0.6, distance_theshold=0.15):
@@ -96,12 +96,12 @@ def findSigns(image, contours, threshold=0.6, distance_theshold=0.15):
             top, left = np.amin(coordinate, axis=0)
             right, bottom = np.amax(coordinate, axis = 0)
             coordinates.append([(top-2,left-2),(right+1,bottom+1)])
-    print(coordinates)
+    #print(coordinates)
     return signs, coordinates
 
 ### Final function to get list of signs, and a list of their coordinates
 
-def panel_detector(path='/Users/chrisphung/Desktop/Bh36Ed4HBJatMpSNnFTgTw.jpg'):
+def panel_detector(path='/data/test_images/French-Road-Signs.jpg'):
     image = cv2.imread(path) #that includes the conversion
     processed_image = preprocess_image(image)
     processed_image_ = removeSmallComponents(processed_image, threshold=300)
@@ -116,64 +116,3 @@ for coordinate in coordinates:
     image = cv2.rectangle(image,coordinate[0],coordinate[1],(0,255,0),3) #coordinate[0] #x1y1 #coordinate[1] #x2y2
 plt.imshow(image)
 plt.show()"""
-
-import tensorflow as tf
-import requests
-from io import BytesIO
-from PIL import Image, ImageDraw, ImageColor, ImageFont
-import matplotlib.pyplot as plt
-import pandas as pd
-
-def gaussian_blur_canny(directory = '/data/test_images/French-Road-Signs.png'):
-    img = cv2.imread(directory) #that includes the conversion
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert the fram to grayscale for edge detection
-    blurred = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=0.5) # Apply Gaussian blur to reduce noise and smoothen edges
-    image_edged = cv2.Canny(blurred, 400, 750) # Perform Canny edge detection
-    return img, image_edged
-
-def houghlines_coordinates(image_edged):
-    #Drawlines to cover area to box
-    lines = cv2.HoughLinesP(
-        image=image_edged,
-        rho=1,
-        theta=(np.pi/180),
-        threshold=20)
-
-    return lines
-
-def box_coordinates(lines):
-    df = []
-    for i in range(len(lines)):
-        df.append(lines[i][0])
-    df = pd.DataFrame(df)
-    df = df.rename(columns={0: "x1", 1: "y1", 2: "x2", 3: "y2"})
-
-    #get top left coordinates
-    x1_y1 = []
-    x1_y1.append(df["x1"].min())
-    x1_y1.append(df["y1"].min())
-
-    #get bottom right coordinates
-    x2_y2 = []
-    x2_y2.append(df["x1"].max())
-    x2_y2.append(df["y1"].max())
-
-    return x1_y1, x2_y2
-
-def box_placer(img, x1_y1, x2_y2):
-    object_identifier = cv2.rectangle(img,x1_y1,x2_y2,(0,255,0),3) #place the box
-    return object_identifier
-
-def identified_object_cropper(img, x1_y1, x2_y2):
-    cropped_img = img[x1_y1[1]:x2_y2[1], x1_y1[0]:x2_y2[0]] #img[y1:y2, x1:x2] crop the image
-    plt.imshow(cropped_img)
-    plt.show()
-
-def object_detector(directory = 'Autonomous-ai/data/test_images/French-Road-Signs.jpg'):
-    img, image_edged = gaussian_blur_canny(directory)
-    lines = houghlines_coordinates(image_edged)
-    x1_y1, x2_y2 = box_coordinates(lines)
-    identified_object_cropper(img, x1_y1, x2_y2)
-
-if __name__ == '__main__':
-    object_detector()
